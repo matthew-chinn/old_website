de6685206fde75b577586e8386a954bed0884100
Matthew Chinn <mattchinn96@gmail.com>
2015-05-07 10:13:39 -0700

    no text decorations

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    var count = 0;
+    $('a').mouseenter(function(){
+        $(this).fadeTo("300", 1);
+    });
+    $('a').mouseleave(function(){
+        $(this).fadeTo("300", .5);
+    });
+    $('#button').click(function(){
+          $('#des').fadeToggle('400');
+          $('#des2').fadeToggle('500');
+    });
+    $('#middle').click(function(){
+        if(count === 0)
+        {
+            $('#troll').append("<img src='face.jpg'>");
+            count++;
+        }
+    });
+});
