64de3b5cc8740565b60b94ad28364c2a8338fc31
Matthew Chinn <mattchinn96@gmail.com>
2015-07-20 17:55:45 -0700

    many updates including homepage animations

---
+++ about.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('#buttons').animate({
+        position:'fixed',
+        right:'50px',
+        top:'30px'
+    }, "slow");
+    
+});
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    var count = 0;
+    /*var count = 0;
     $('#button').click(function(){
           $('#des').fadeToggle('400');
           $('#des2').fadeToggle('500');
@@ -10,5 +10,30 @@ $(document).ready(function(){
             $('#troll').append("<img src='face.jpg'>");
             count++;
         }
-    });
+    });*/
+        $('#name').animate({fontSize: '50px' }, "slow");
+        $('#about').animate({
+            position:'absolute',
+            top:'180px',
+            left:'280px'
+        }, "slow");
+        $('#contact').animate({
+            position:'absolute',
+            top:'180px',
+            right:'260px'
+        }, "slow");
+        $('#resume').animate({
+            position:'absolute',
+            bottom:'180px',
+            left:'280px'
+        }, "slow");
+        $('#game').animate({
+            position:'absolute',
+            bottom:'180px',
+            right:'280px'
+        }, "slow");
+
+
+
+    
 });
