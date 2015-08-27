dd78ec93e2f8d8713f772c66f504075109afb0ed
Matthew Chinn <mattchinn96@gmail.com>
2015-08-27 14:32:41 -0700

    finished 3rd part of animation

--- animate.js
+++ animate.js
@@ -1,68 +1,93 @@
 var FORWARD = true;
 var BACKWARD = false;
+var animation1, animation2, bodyWidth, height, leftWheel, 
+    leftInnerWheel, rightWheel, rightInnerWheel;
 
 $(document).ready(function(){
-    var animation1 = $('#animation1');
-    var animation2 = $('#animation2');
-    var time = 3000;
-    var bodyWidth = parseInt($('body').width());
-    var distance = bodyWidth - 170;
-    var height = $(document).height();
-    var distanceBack = distance - 100;
-    var timeBack = 2000;
-    animation1.velocity({translateX:""+distance+"px"}, time)
-       .velocity({translateX:""+distanceBack+"px"}, 
-       {duration:timeBack})
+    height = $(document).height();
+    bodyWidth = parseInt($('body').width());
+    animation1 = $('#animation1');
+    animation2 = $('#animation2');
+    leftWheel = $('#animation1 #leftwheel');
+    leftInnerWheel = $('#animation1 #leftinnerwheel');
+    rightWheel = $('#animation1 #rightwheel');
+    rightInnerWheel = $('#animation1 #rightinnerwheel');
+    
+    var animationCount = 0;
+    animationCount = animate(animationCount);
 
-    animation2.velocity({translateX:""+distance+"px"}, 
-        {duration:time})
-        .velocity({translateX:""+distanceBack+"px"}, 
-        {duration:timeBack});
+    $('#buttons a').click(function(){
+        animationCount = animate(animationCount);
+    });
 
-    var leftWheel = $('#leftwheel');
-    var leftInnerWheel = $('#leftinnerwheel');
-    rotateWheel(leftWheel, leftInnerWheel, time, distance, 3, FORWARD);
-    var rightWheel = $('#rightwheel');
-    var rightInnerWheel = $('#rightinnerwheel');
-    rotateWheel(rightWheel, rightInnerWheel, time, distance, 0, FORWARD);
+});
 
-    //after impact go backwards
-    rotateWheel(leftWheel, leftInnerWheel, timeBack, distanceBack, 0,
-        BACKWARD);
-    rotateWheel(rightWheel, rightInnerWheel, timeBack, distanceBack, 9,
-        BACKWARD);
+//animate according to what number in sequence
+function animate(count)
+{
+    switch(count){
+        case 0: //initial forward
+            var distance = bodyWidth - 170; //for initial travel forward
+            var distanceBack = distance - 100;
+            var timeForward = 3000; //for initial distance
+            var timeBack = 2000;
+            
+            animation1.velocity({translateX:""+distance+"px"}, timeForward)
+               .velocity({translateX:""+distanceBack+"px"}, 
+               {duration:timeBack})
 
-    //freefall
-    $('.startUp a').click(function(){
-        var rotate = 80; //angle to start at
-        $('.startUp').removeClass("startUp");
-        animation1.velocity("fadeOut", 1)
-                  .velocity({translateX:""+(bodyWidth+100)+"px",
-                             translateY:""+height+"px"})
-                  .velocity({rotateZ:""+rotate+"deg"});
-        flipWagon();
-        animation2.velocity({translateY:""+height*2+"px"}, 
-        {duration:1000, visibility:"visible", display:"none"});
+            animation2.velocity({translateX:""+distance+"px"}, 
+                {duration:timeForward})
+                .velocity({translateX:""+distanceBack+"px"}, 
+                {duration:timeBack});
 
-        $('#buttons a').click(function(){
-            //make wagon go up rainbow
+            rotateWheel(leftWheel, leftInnerWheel, timeForward, 
+                distance, 3, FORWARD);
+            rotateWheel(rightWheel, rightInnerWheel, timeForward, 
+                distance, 0, FORWARD);
+
+            //after impact go backwards
+            rotateWheel(leftWheel, leftInnerWheel, timeBack, distanceBack, 0,
+                BACKWARD);
+            rotateWheel(rightWheel, rightInnerWheel, timeBack, distanceBack, 9,
+                BACKWARD);
+            break;
+
+        case 1: //wagon freefall
+            var rotate = 80; //angle to start at
+            var fallTime = 1000;
+            var move1ToX = bodyWidth;
+            var move1ToY = height;
+            animation1.velocity("fadeOut", 1)
+                      .velocity({translateX:""+move1ToX+"px",
+                                 translateY:""+move1ToY+"px"})
+                      .velocity({rotateZ:""+rotate+"deg"});
+            flipWagon();
+            animation2.velocity({translateY:""+height*2+"px"}, 
+            {duration:fallTime, visibility:"visible", display:"none"});
+            break;
+
+        case 2: //wagon fly up
             var rotateTo = 0; //amount to change angle into
-            var time = 10000; //time rainbow shimmers
+            var time = 5000; //time rainbow shimmers
+            var finalX = 0;
+            var finalY = -300;
+            makeRainbow($('.wagon'), time);
+            rotateWheel(leftWheel, leftInnerWheel, time, bodyWidth, 3,
+                BACKWARD);
+            rotateWheel(rightWheel, rightInnerWheel, time, bodyWidth, 0,
+                BACKWARD);
             animation1.velocity("fadeIn", 1);
-            makeRainbow($('#rainbow'), time);
-            animation1.velocity({translateX:""+(bodyWidth/2+100)+"px",
-                                 translateY:"-300px"}, time)
-                   //   .velocity({translateX:"-200px",
-                   //              translateY:"-200px"}, time);
+            animation1.velocity({translateX:""+finalX+"px",
+                                 translateY:""+finalY+"px"}, time)
             setTimeout(function(){
                 animation1.velocity({rotateZ:""+rotateTo+"deg"}, 
                         {duration:time, queue:false});
             }, time/5);
-            $(this).unbind();
-        });
-    });
-
-});
+            break;
+    }
+    return ++count;
+}
 
 function makeRainbow(element, time)
 {
@@ -80,7 +105,7 @@ function makeRainbow(element, time)
                .velocity({fill:"#6600CC"}, transition);
         time-=transition*6;
     }
-    element.velocity({fill:"#ffffff"}, {display:"none"}); //fade out to white
+    element.velocity({fill:"url(#wagonColor)"}); //fade out to white
 }
 
 function flipWagon()
@@ -96,12 +121,11 @@ function flipWagon()
 function rotateWheel(wheel, innerWheel, time, distance, beginCount, direction){
     var x = parseInt(wheel.attr("cx"));
     var y = parseInt(wheel.attr("cy"));
-    var r = parseInt(innerWheel.attr("cx") - x);
+    var r = Math.abs(parseInt(innerWheel.attr("cx")) - x);
     if(r === 0) //beginCount === 3 or 9
     {
-        r = parseInt(innerWheel.attr("cy") - y);
+        r = Math.abs(parseInt(innerWheel.attr("cy")) - y);
     }
-
     var radianPiece = Math.PI / 6;
 
     var count = beginCount; //count of radian pieces wheel has gone
