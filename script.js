7f057cfc94b82bcd1387ec7f111aad34a6947af1
Matthew Chinn <mattchinn96@gmail.com>
2015-09-10 17:11:52 -0700

    test out media query

--- script.js
+++ script.js
@@ -36,60 +36,64 @@ var transitionHome = function(){
 	home.fadeOut("500");
 }
 
+var mq = window.matchMedia( "(max-width:640px)" );
+
 $(document).ready(function(){
-    about = $('#about');
-    contact = $('#contact');
-    resume = $('#resume');
-    project = $('#project');
-    runnerbutton = $('#runnerbutton');
-    home = $('#home');
-    backtotop = $('#backtotop');
-    buttons = $('#buttons');
+    if(mq.matches){ //for a desktop browser
+        about = $('#about');
+        contact = $('#contact');
+        resume = $('#resume');
+        project = $('#project');
+        runnerbutton = $('#runnerbutton');
+        home = $('#home');
+        backtotop = $('#backtotop');
+        buttons = $('#buttons');
 
-	$('.startUp div:not(#runnerbutton) a').click(function(){
-		about.velocity({top: '50px', right:'50px'});
-		contact.velocity({top: '80px', right:'50px'});
-		resume.velocity({top: '110px', right:'50px'}); 
-		project.velocity({top: '140px', right:'50px'});
-        runnerbutton.velocity({top: '170px', right:'50px'});
-		home.fadeIn("500");
-		buttons.removeClass("startUp");
-	});
+        $('.startUp div:not(#runnerbutton) a').click(function(){
+            about.velocity({top: '50px', right:'50px'});
+            contact.velocity({top: '80px', right:'50px'});
+            resume.velocity({top: '110px', right:'50px'}); 
+            project.velocity({top: '140px', right:'50px'});
+            runnerbutton.velocity({top: '170px', right:'50px'});
+            home.fadeIn("500");
+            buttons.removeClass("startUp");
+        });
 
-    $("#buttons div:not(#runnerbutton) a").click(function(){
-		$('.active').fadeOut("500").removeClass("active");
-    });
+        $("#buttons div:not(#runnerbutton) a").click(function(){
+            $('.active').fadeOut("500").removeClass("active");
+        });
 
 
-	about.click(function(){
-		$('#aboutpage').fadeIn("500").addClass("active");
-		backtotop.fadeOut("500");
-	});
+        about.click(function(){
+            $('#aboutpage').fadeIn("500").addClass("active");
+            backtotop.fadeOut("500");
+        });
 
-	contact.click(function(){
-		$('#contactpage').fadeIn("500").addClass("active");
-		backtotop.fadeOut("500");
-	});
+        contact.click(function(){
+            $('#contactpage').fadeIn("500").addClass("active");
+            backtotop.fadeOut("500");
+        });
 
-	resume.click(function(){
-		$('#resumepage').fadeIn("500").addClass("active");
-		backtotop.fadeIn("500");
-	});
+        resume.click(function(){
+            $('#resumepage').fadeIn("500").addClass("active");
+            backtotop.fadeIn("500");
+        });
 
-	home.click(function(){
-		$('#homepage').fadeIn("500").addClass("active");
-		buttons.addClass("startUp");
-		backtotop.fadeOut("500");
-        $('#name').css("fontSize", "24px");
-		transitionHome();
-	});
+        home.click(function(){
+            $('#homepage').fadeIn("500").addClass("active");
+            buttons.addClass("startUp");
+            backtotop.fadeOut("500");
+            $('#name').css("fontSize", "24px");
+            transitionHome();
+        });
 
-    project.click(function(){
-		$('#projectpage').fadeIn("500").addClass("active");
-		backtotop.fadeIn("500");
-    });
+        project.click(function(){
+            $('#projectpage').fadeIn("500").addClass("active");
+            backtotop.fadeIn("500");
+        });
 
-    transitionHome();
+        transitionHome();
+    }
 
 });
 
